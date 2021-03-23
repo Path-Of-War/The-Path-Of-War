d325a6987837c5551460ffb8a403c5fe89b6659c
Hugo <hugo.quitton@gmail.com>
2021-03-23 16:24:47 +0100

    Camera rotation done
    Camera can now rotate around the player

--- Path of War/Assets/Scripts/CamFollow.cs
+++ Path of War/Assets/Scripts/CamFollow.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class CamFollow : MonoBehaviour
 {
     Player p;
-    Transform myTransform;
     Vector3 offset;
+    public float smoothFactor = 0.5f;
+
+    public float rotationSpeed;
 
     void Start()
     {
         p = Player.instance;
-        myTransform = transform;
-        offset = p.transform.position + myTransform.position;
+        offset = transform.position - p.transform.position;
 
     }
 
     void LateUpdate()
     {
+        Quaternion camTurnAngle;
+        camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse ScrollWheel") * rotationSpeed, Vector3.up);
+        offset = camTurnAngle * offset;
 
-        myTransform.position = p.transform.position + offset;
+        Vector3 newPos = p.transform.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+        transform.LookAt(p.transform.position);
 
     }
+
 }
