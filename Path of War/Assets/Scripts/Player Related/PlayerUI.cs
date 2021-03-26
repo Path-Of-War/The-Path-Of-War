401d07c338c794b9b250afeb7fe973d723c6dc2a
Hugo <hugo.quitton@gmail.com>
2021-03-26 08:52:30 +0100

    mana bar

--- Path of War/Assets/Scripts/Player Related/PlayerUI.cs
+++ Path of War/Assets/Scripts/Player Related/PlayerUI.cs
@@ -7,9 +7,13 @@ public class PlayerUI : MonoBehaviour
 {
     public Slider playerHealthBar;
 
+    public Slider playerManaBar;
+
     private void Update()
     {
         playerHealthBar.maxValue = Player.instance.maxHealth;
         playerHealthBar.value = Player.instance.currentHealth;
+        playerManaBar.maxValue = Player.instance.maxMana;
+        playerManaBar.value = Player.instance.currentMana;
     }
 }
